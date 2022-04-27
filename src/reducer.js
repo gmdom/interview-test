6b53f8ba25bc45e4a596d3b176db10064b17bdbf
gmdom <rafael.gm2@gmail.com>
2022-04-27 13:25:41 -0400

    update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,21 @@
-export default function redurer(state, action) {
+export default function reducer(state, action) {
   switch (action.type) {
+    case 'ADDED': {
+      return [...state, { id: Date.now(), task: action.task, done: false }];
+    }
+    case 'UPDATED': {
+      return state.map((todo) =>
+        todo.id !== action.id ? todo : { ...todo, task: action.task }
+      );
+    }
+    case 'TOGGLE': {
+      return state.map((todo) =>
+        todo.id !== action.id ? todo : { ...todo, done: !todo.done }
+      );
+    }
+    case 'DELETED': {
+      return state.filter((todo) => todo.id !== action.id);
+    }
     default: {
       return state;
     }
