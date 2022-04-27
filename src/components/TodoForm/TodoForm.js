8569f311686b297525ff4e0e1cc05c0c6e4edb08
gmdom <rafael.gm2@gmail.com>
2022-04-27 14:17:56 -0400

    make TodoForm gets focus on start

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import styles from './Todo.module.css';
 export default function TodoForm({ addTodo }) {
   const [task, setTask] = useState('');
+  const inputRef = useRef(null);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (task) {
@@ -12,10 +13,13 @@ export default function TodoForm({ addTodo }) {
   const handleChange = (e) => {
     setTask(e.target.value);
   };
-
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <input
+        ref={inputRef}
         type="text"
         value={task}
         onChange={handleChange}
